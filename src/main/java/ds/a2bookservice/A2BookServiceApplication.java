eb281d07f9f4deac2991908c957a9b58fa5aaf36
harsha <durvasula.sampath@gmail.com>
2025-04-01 15:53:30 -0400

    book service pushing

--- src/main/java/ds/a2bookservice/A2BookServiceApplication.java
+++ src/main/java/ds/a2bookservice/A2BookServiceApplication.java
@@ -2,8 +2,10 @@ package ds.a2bookservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"controllers", "services", "repositories", "models"})
 public class A2BookServiceApplication {
 
     public static void main(String[] args) {
